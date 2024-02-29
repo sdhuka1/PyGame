a1412bbaa1d001dd1cf05584c6ace1fe711e1ebc
Shayan <Shayan_Dhuka1@baylor.edu>
2024-02-28 23:13:13 -0600

    Added dynamic settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -75,6 +75,7 @@ class AlienInvasion:
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_activate:
             #This resets the game statistics
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_activate = True
             #This hides the mouse cursor
@@ -131,6 +132,7 @@ class AlienInvasion:
             #This detroys existing bullets and creates a new fleet
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
     
     def _update_aliens(self):
         #This checks if the fleet is at an edge, then updates the position of all the aliens in the fleet
