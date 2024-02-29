012d06aae0fd084242456cd564dd64398940e190
Shayan <Shayan_Dhuka1@baylor.edu>
2024-02-28 20:25:52 -0600

    Added speed limit to ship and limited ship range

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         #This initializes the ship and sets its starting position
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #This loads the ship image and gets its rect
@@ -15,16 +16,23 @@ class Ship:
         #This starts each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #This stores a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+        
         #This serves as a movement flag
         self.moving_right = False
         self.moving_left = False
     
     def update(self):
         #This updates the ship's position based on the movement flag
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        #This updates the ship's x value and not the rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        
+        #This updates the rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         #This draws the ship at its current location
